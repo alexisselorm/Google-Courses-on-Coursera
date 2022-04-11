7edf193a331b0daaf196b9e8ab5ad430da705dae
Alexis Selorm Gbeckor-Kove <agbeckor2@gmail.com>
2022-04-11 18:11:51 +0100

    Backup process

--- Automation With Python/Regular Expressions/regex.py
+++ Automation With Python/Regular Expressions/regex.py
@@ -1,3 +1,4 @@
+
 import re
 
 
@@ -89,3 +90,95 @@ print(repeating_letter_a("banana")) # True
 print(repeating_letter_a("pineapple")) # False
 print(repeating_letter_a("Animal Kingdom")) # True
 print(repeating_letter_a("A is for apple")) # True
+
+# Escape characters with \
+print(re.search(r"\.com", "mydomain.com"))
+
+#  \w matches any alphanumeric character including letters, numbers, and underscores
+print(re.search(r"\w*","This is an example"))
+print(re.search(r"\w*", "This_is_Another_example"))
+# There's also \d for matching digits, \s for matching whitespace characters like space, tab or new line, \b for word boundaries and a few others
+
+
+# # Question
+# Fill in the code to check if the text passed has at least 2 groups of alphanumeric characters (including letters, numbers, and underscores) separated by one or more whitespace characters.
+def check_character_groups(text):
+  result = re.search(r"\w \w", text)
+  return result != None
+
+print(check_character_groups("One")) # False
+print(check_character_groups("123  Ready Set GO")) # True
+print(check_character_groups("username user_01")) # True
+print(check_character_groups("shopping_list: milk, bread, eggs.")) # False
+
+
+# To match something but begins and ends with(more strictly), use the circumflex and $ sogns to make it more strict.
+print(re.search(r"^A.*a$","Argentina"))
+print(re.search(r"^A.*a$","Azerbaijan"))
+
+# To construct a string that is a valid variable name in Python
+pattern = r"^[a-zA-Z_][a-zA-Z0-9_]*$"
+print(re.search(pattern,"_this_is_a_valid_variable"))
+print(re.search(pattern,"is this valid or naa"))
+print(re.search(pattern,"2what_about_This"))
+
+
+# Fill in the code to check if the text passed looks like a standard sentence, meaning that it starts with an uppercase letter, followed by at least some lowercase letters or a space, and ends with a period, question mark, or exclamation point. 
+def check_sentence(text):
+      result = re.search(r"^[A-Z].*[a-zA-Z ][.?!]$", text)
+      return result != None
+
+print(check_sentence("Is this is a sentence?")) # True
+print(check_sentence("is this is a sentence?")) # False
+print(check_sentence("Hello")) # False
+print(check_sentence("1-2-3-GO!")) # False
+print(check_sentence("A star is born.")) # True
+
+# The check_web_address function checks if the text passed qualifies as a top-level web address, meaning that it contains alphanumeric characters (which includes letters, numbers, and underscores), as well as periods, dashes, and a plus sign, followed by a period and a character-only top-level domain such as ".com", ".info", ".edu", etc. Fill in the regular expression to do that, using escape characters, wildcards, repetition qualifiers, beginning and end-of-line characters, and character classes.
+def check_web_address(text):
+      pattern = r"^[\w]*[\.\-\+][^/@]*$"
+      result = re.search(pattern, text)
+      return result != None
+
+print(check_web_address("gmail.com")) # True
+print(check_web_address("www@google")) # False
+print(check_web_address("www.Coursera.org")) # True
+print(check_web_address("web-address.com/homepage")) # False
+print(check_web_address("My_Favorite-Blog.US")) # True
+
+# Question 2
+# The check_time function checks for the time format of a 12-hour clock, as follows: the hour is between 1 and 12, with no leading zero, followed by a colon, then minutes between 00 and 59, then an optional space, and then AM or PM, in upper or lower case. Fill in the regular expression to do that. How many of the concepts that you just learned can you use here?
+def check_time(text):
+      pattern = r'(1[012]|[1-9]):[0-5][0-9][ ]{0,1}?(am|pm|AM|PM)'
+      result = re.search(pattern, text)
+      return result != None
+
+print(check_time("12:45pm")) # True
+print(check_time("9:59 AM")) # True
+print(check_time("6:60am")) # False
+print(check_time("five o'clock")) # False
+
+
+# The contains_acronym function checks the text for the presence of 2 or more characters or digits surrounded by parentheses, with at least the first character in uppercase (if it's a letter), returning True if the condition is met, or False otherwise. For example, "Instant messaging (IM) is a set of communication technologies used for text-based communication" should return True since (IM) satisfies the match conditions." Fill in the regular expression in this function: 
+def contains_acronym(text):
+      pattern = r"\(\w.*\w\)" 
+      result = re.search(pattern, text)
+      return result != None
+
+print(contains_acronym("Instant messaging (IM) is a set of communication technologies used for text-based communication")) # True
+print(contains_acronym("American Standard Code for Information Interchange (ASCII) is a character encoding standard for electronic communication")) # True
+print(contains_acronym("Please do NOT enter without permission!")) # False
+print(contains_acronym("PostScript is a fourth-generation programming language (4GL)")) # True
+print(contains_acronym("Have fun using a self-contained underwater breathing apparatus (Scuba)!")) # True
+
+
+# Question 6
+# Fill in the code to check if the text passed includes a possible U.S. zip code, formatted as follows: exactly 5 digits, and sometimes, but not always, followed by a dash with 4 more digits. The zip code needs to be preceded by at least one space, and cannot be at the start of the text.
+def check_zip_code (text):
+      result = re.search(r' \d{5}(?:-\d{4})?', text)
+      return result != None
+
+print(check_zip_code("The zip codes for New York are 10001 thru 11104.")) # True
+print(check_zip_code("90210 is a TV show")) # False
+print(check_zip_code("Their address is: 123 Main Street, Anytown, AZ 85258-0001.")) # True
+print(check_zip_code("The Parliament of Canada is at 111 Wellington St, Ottawa, ON K1A0A9.")) # False 
